7a8509a17ee90c87e91fdf31cd219f5e5c8bbb90
Disguyranger <46687995+Disguyranger@users.noreply.github.com>
2021-12-01 20:58:11 -0500

    Updated with narrative
    Comments attached

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import sys, os, time, pandas as pd, matplotlib as plt, numpy as np
 
-## This is how we will work through with minimal user intervention to reduce error
-months = {1 : ["January", "February", "March", "April"], 
-2 : ["February", "March", "April", "May"],
-3 : ["March", "April", "May", "June"], 
-4 : ["April", "May", "June", "July"], 
-5 : ["May", "June", "July", "August"],
-6 : ["June", "July", "August", "September"], 
-7 : ["July", "August", "September", "October"], 
-8 : ["August", "September", "October", "November"]}
+# Set of months that will be read
+months = {1 : ["January", "February", "March", "April", 50], 
+2 : ["February", "March", "April", "May", 60],
+3 : ["March", "April", "May", "June", 70], 
+4 : ["April", "May", "June", "July", 80], 
+5 : ["May", "June", "July", "August", 90],
+6 : ["June", "July", "August", "September", 100], 
+7 : ["July", "August", "September", "October", 100], 
+8 : ["August", "September", "October", "November", 100]}
 
-
+# Only user input to tell us which set of information is being run
 startMonth = 1 #int(input("Which iteration is this: "))
 
+# Read in all Excel files through Pandas given from startMonth -
 firstMonth = pd.read_excel(f'{months[startMonth][0]}.xlsx')
 secondMonth = pd.read_excel(f'{months[startMonth][1]}.xlsx')
 thirdMonth = pd.read_excel(f'{months[startMonth][2]}.xlsx')
 fourthMonth = pd.read_excel(f'{months[startMonth][3]}.xlsx')
 issuedGroup = pd.read_excel("Saved_Repo.xlsx")
 
+# If issuedGroup is populated, find all names that are the same and drop rows
 
+# Iterate through all names and create new rows with total1 divided by actualTotal1 and total2 % actualTotal2
 
+# Save to new Excel file with all names of new group. Should not exist!
+
+# Append 'Saved_Repo.xlsx' with dataframe from previous Excel file
+
+# Open files for viewing (Saved_Repo.xlsx and {newExcelFileName}.xlsx)
 
