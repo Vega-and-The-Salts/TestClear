1650e63c351a231b690b03497e6776b2c401ab47
Disguyranger <46687995+Disguyranger@users.noreply.github.com>
2021-12-01 20:07:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import sys, os, time, pandas as pd, matplotlib as plt, numpy as np
+#"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+months = {1 : ["January", "February", "March", "April", "May"],
+2 : ["February", "March", "April", "May", "June"],
+3 : ["March", "April", "May", "June", "July"],
+4 : ["April", "May", "June", "July", "August"],
+5 : ["May", "June", "July", "August", "September"],
+6 : ["June", "July", "August", "September", "October"],
+7 : ["July", "August", "September", "October", "November"],
+8 : ["August", "September", "October", "November", "December"]}
 
-def funtA():
-    pass
 
-funtA()
+
+startMonth = input("Which iteration is this: ")
+
+dataFirstMonth = pd.read_excel("AprilData.xlsx")
+
+
+
+
+
