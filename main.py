94296cb1e92ff12cf56d308958f47e6111ce4830
Disguyranger <46687995+Disguyranger@users.noreply.github.com>
2021-12-19 13:06:38 -0500

    update

--- main.py
+++ main.py
@@ -1,59 +1,16 @@
-import sys, os, time, pandas as pd, matplotlib as plt, numpy as np
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Set of months that will be read
-months = {1 : ["January", "February", "March", "April", 50], 
-2 : ["February", "March", "April", "May", 60],
-3 : ["March", "April", "May", "June", 70], 
-4 : ["April", "May", "June", "July", 80], 
-5 : ["May", "June", "July", "August", 90],
-6 : ["June", "July", "August", "September", 100], 
-7 : ["July", "August", "September", "October", 100], 
-8 : ["August", "September", "October", "November", 100]}
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
 
-# Only user input to tell us which set of information is being run
-startMonth = 1 #int(input("Which iteration is this: "))
-
-# Read in all Excel files through Pandas given from startMonth -
-firstMonth = pd.read_excel(f'{months[startMonth][0]}.xlsx')
-secondMonth = pd.read_excel(f'{months[startMonth][1]}.xlsx')
-thirdMonth = pd.read_excel(f'{months[startMonth][2]}.xlsx')
-fourthMonth = pd.read_excel(f'{months[startMonth][3]}.xlsx')
-issuedGroup = pd.read_excel("Saved_Repo.xlsx")
-
-# Check to see if issuedGroup is populated
-if issuedGroup.empty:
-    pass
-    # Look at each month and add the sum of all sum1+2+3 
-    
-    # Divide the gathered information against actualTotal1 to 
-    # get the Percentage by creating new column 
-        ## IF EVER INFORMATION IS OVER 100% CHECK INFORMATION
-
-    # Look at each month and add the sum of all sum01+02+03
-        ## IF EVER INFORMATION IS OVER 100% CHECK INFORMATION
-
-    # Divide the gathered information against actualTotal01 to 
-    # get the Percentage by creating new column
-
-    # If data falls below threshhold of startMonth[months][-1] add 
-
-    # Write information to Saved_Repo.xlsx
-
-    #Write information to new Excel file
-
-    #Open new Excel file for view
-
-
-# If issuedGroup is populated, find all names that are the same and drop rows
-
-# Iterate through all names and create new rows with total1 divided by actualTotal1 and total2 % actualTotal2
-
-# Save to new Excel file with all names of new group. Should not exist!
-
-# Append 'Saved_Repo.xlsx' with dataframe from previous Excel file
-
-# Open files for viewing (Saved_Repo.xlsx and {newExcelFileName}.xlsx)
-
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
